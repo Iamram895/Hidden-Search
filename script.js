62f7c3ee47d606f685dd0042e18af6c5fb7aaa83
shriram <choudharyshriram53@gmail.com>
2022-11-10 13:02:58 +0530

    files is added

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
-const search = document.querySelector('.search')
-const btns = document.querySelector('.btn')
-const input = document.querySelector('.input')
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
 
-btns.addEventListener('click', () => {
-    search.classList.toggle('active')
-    input.focus()
-})
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  console.log(circles.length);
+  console.log(currentActive);
+
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    // console.log(idx);
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
+
